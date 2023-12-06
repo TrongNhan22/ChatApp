cf82b7a4b176b77af557f783c763dfc70748d3ae
Phúc Phan Thành <21522477@ms.uit.edu.vn>
2023-12-06 20:15:46 +0700

    #1 added Message.cs model

---
+++ ChatApp/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models
+{
+    public class Message
+    {
+        [Key]
+        public string Id { get; set; }
+        public string? SenderId { get; set; }
+        public string? ReceiverId { get; set; }
+        public DateTime? Date { get; set; }
+        public string? content { get; set; }
+        public string Media { get; set; }
+    }
+}
