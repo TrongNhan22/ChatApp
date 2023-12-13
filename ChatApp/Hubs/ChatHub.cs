0efd27e2e16e89a3211f631d73ac9942231b3a75
Phúc Phan Thành <21522477@ms.uit.edu.vn>
2023-12-13 20:43:35 +0700

    #3 updated chathub and chatpage

--- ChatApp/Hubs/ChatHub.cs
+++ ChatApp/Hubs/ChatHub.cs
@@ -1,14 +1,52 @@
 ﻿using ChatApp.Models;
 using Microsoft.AspNetCore.SignalR;
+using SignalRChatDemo.Models;
 
 namespace ChatApp.Hubs
 {
     public class ChatHub : Hub
     {
+        private readonly static ConnectionMapping <string> _connections = new();
         public async Task Send(Message message)
         {
             // Call the broadcastMessage method to update clients.
             await Clients.All.SendAsync("broadcastMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var userList = _connections.GetUsers();
+            await Clients.Client(Context.ConnectionId).SendAsync("ReceiveCurrentUserList", _connections.GetUsers());
+
+            string connectionId = Context.User?.Identity?.Name ?? Context.ConnectionId;
+            _connections.Add(connectionId, connectionId);
+
+            //Broadcast a message to all chat announcing a user has joined
+            var broadcastMessage = new Message
+            {
+                Id = "",
+                SenderId = "SYSTEM",
+                ReceiverId = "",
+                Content = $"{connectionId} đã tham gia chat",
+            };
+            await Send(broadcastMessage);
+            await Clients.All.SendAsync("UserJoined", connectionId);
+        }
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string connectionId = Context.User?.Identity?.Name ?? Context.ConnectionId;
+            _connections.Remove(connectionId, connectionId);
+
+            //Broadcast a message to all chat announcing a user has joined
+            var broadcastMessage = new Message
+            {
+                Id = "",
+                SenderId = "SYSTEM",
+                ReceiverId = "",
+                Content = $"{connectionId} đã rời khỏi chat",
+            };
+            await Send(broadcastMessage);
+            await Clients.All.SendAsync("UserLeft", connectionId);
         }
     }
 }
---
+++ ChatApp/wwwroot/lib/helpers/createChatBubble.js
@@ -0,0 +1,38 @@
+﻿function createChatBubble(message, name) {
+    return (message.senderId == "SYSTEM")
+        ?
+        `
+                <div class="d-flex flex-row justify-content-center" >
+                    <div>
+                        <p class="small p-2 ms-3 mb-1 rounded-3" style = "background-color: #a3ffb4;" >
+                            ${message.content}
+                        </p>
+             
+                    </div>
+                </div>
+        `
+        : (message.senderId === name)
+            ?
+            `
+                    <div class="d-flex flex-row justify-content-end" >
+                        <div>
+                            <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary" >
+                                ${message.content}
+                            </p>
+                            <p class="small me-3 mb-3 rounded-3 text-muted"> 12: 00 PM | Aug 13 </p>
+            `
+            :
+            `
+                    <div class="d-flex flex-row justify-content-start" >
+                        <p>${message.senderId} </p>
+                        <div>
+                            <p class="small p-2 ms-3 mb-1 rounded-3" style = "background-color: #f5f6f7;" >
+                                ${message.content}
+                            </p>
+                            <p class="small ms-3 mb-3 rounded-3 text-muted float-end">
+                                12: 00 PM | Aug 13
+                            </p>
+                        </div>
+                    </div>
+            `;
+}
