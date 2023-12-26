e2557dbd2074a6fd5e3a6d49976b19af0afe02bd
= <=>
2023-12-26 21:59:35 +0700

    test

---
+++ ChatApp/Pages/Sign-up.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatApp.Pages
+{
+    public class Sign_upModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
