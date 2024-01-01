883d48dab9cfb4b9490ca31cb5a010feedb10a69
duytai800 <duytai800@gmail.com>
2024-01-01 15:44:52 +0700

    add controller and model for login

---
+++ ChatApp/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? Avatar { get; set; }
+        public string? Birthday { get; set; }
+        public string? Fullname { get; set; }
+        public int? Gender { get; set; }
+
+    }
+
+}
