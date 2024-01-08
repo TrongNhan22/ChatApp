b8ae628bf087783bc0c716f458c4b9fe80ebf729
= <=>
2024-01-08 22:00:49 +0700

    update tum` lum

---
+++ ChatApp/Interface/IUpdateUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using ChatApp.Models;
+
+namespace ChatApp.Interface
+{
+    public interface IUpdateUserRepository
+    {
+        Task<User> GetUserById(string id);
+
+        void UpdateUser(string id, string fullname, string gender, string birhtday, string email);
+    }
+}
