32a0a9e5e377dbedab28459458bc2ce5899d4130
= <=>
2024-01-09 14:44:05 +0700

    update final 1

--- ChatApp/Controllers/UserController.cs
+++ ChatApp/Controllers/UserController.cs
@@ -28,7 +28,7 @@ namespace ChatApp.Controllers
                 return Redirect("/");
             }
             // If the user does not exist, redirect back to the login page
-            TempData["Error"] = "Email or password is not correct!";
+            TempData["Error"] = "Email hoặc mật khẩu không đúng!";
             return Redirect("/Login");
         }
 
@@ -40,11 +40,11 @@ namespace ChatApp.Controllers
             if (account)
             {
                 // If the user exists, redirect to the home page
-                TempData["Noti"] = "New account was created succcessfully!";
-                return Redirect("/sign-up");
+                TempData["Noti"] = "Tạo tài khoản thành công!";
+                return Redirect("/login");
             }
             // If the user does not exist, redirect back to the login page
-            TempData["Error"] = "Email is already used!";
+            TempData["Error"] = "Email đã được sử dụng!";
             return Redirect("/sign-up");
         }
     }
